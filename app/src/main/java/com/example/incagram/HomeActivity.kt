c5e3eeb8e283539756b93cef0ebe3ad568b6c54c
Santiago Osuna <Santiagoosuna.com>
2025-08-26 18:58:58 -0500

    Ajustes en kotlin + java para funciones del codigo

--- app/src/main/java/com/example/incagram/HomeActivity.kt
+++ app/src/main/java/com/example/incagram/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.example.incagram
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_home)
+
+        val btnComienza = findViewById<Button>(R.id.btn_comienza)
+        btnComienza.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/incagram/SplashActivity.kt
+++ app/src/main/java/com/example/incagram/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.example.incagram
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 3000 // 3 segundos
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+
     }
 }
